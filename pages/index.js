7c57e2be01c506c24c63ad8b13795c0fdbdc8edf
xammyboii <xavieryabados.09@gmail.com>
2023-02-28 14:41:24 -0600

    added submitFormHandler function

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,29 @@
+import { useRef } from "react";
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+    fetch();
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor="email">Your Email Address</label>
-          <input type="email" />
+          <input type="email" id="email" ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor="feedback">Your Feedback</label>
-          <textarea id="feedback" rows='5'></textarea>
+          <textarea id="feedback" rows='5' ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
