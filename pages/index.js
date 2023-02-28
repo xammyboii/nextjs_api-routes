00a74684d6f310fc75ec08a1a364d16186987242
xammyboii <xavieryabados.09@gmail.com>
2023-02-28 14:33:05 -0600

    added form

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,17 @@ function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea id="feedback" rows='5'></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
   );
 }
